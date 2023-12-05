8c3c8dc0a158f99e158b6d7b2f72cc1175c5665d
gottbrecht <simo79j5@stud.kea.dk>
2023-12-05 08:47:27 +0100

    hover

--- styles/hover.js
+++ styles/hover.js
@@ -1,20 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const dropdownToggle = document.querySelector('.dropdown-toggle');
-    const dropdownMenu = document.querySelector('.dropdown-menu');
-
-    let isDropdownOpen = false;
-
-    dropdownToggle.addEventListener('mouseover', function () {
-        if (!isDropdownOpen) {
-            dropdownToggle.click();
-            isDropdownOpen = true;
-        }
-    });
-
-    dropdownMenu.addEventListener('mouseleave', function () {
-        if (isDropdownOpen) {
-            dropdownToggle.click();
-            isDropdownOpen = false;
-        }
-    });
+    
 });
