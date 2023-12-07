f033a03f3790a87fae69c956e7a377d2f22abf6c
Tohasim <toreh.simonsen@live.dk>
2023-12-07 16:04:14 +0100

    Added correct picture and did some small fixes

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,71 +1,79 @@
-import { API_URL } from '../../settings.js'
-import { handleHttpErrors } from '../../utils.js'
+import { API_URL } from "../../settings.js";
+import { handleHttpErrors } from "../../utils.js";
 
-let responseStatus
+let responseStatus;
 export function initLogin(match) {
-  const msg = match?.params?.msg
-  if (msg) {
-    document.getElementById("signedup-msg").innerText = msg || ""
-  }
-  responseStatus = document.getElementById("response-status")
-  document.getElementById("login-btn").addEventListener("click", login) 
+	const msg = match?.params?.msg;
+	if (msg) {
+		document.getElementById("signedup-msg").innerText = msg || "";
+	}
+	responseStatus = document.getElementById("response-status");
+	document.getElementById("login-btn").addEventListener("click", login);
 }
 
 async function login() {
-  responseStatus.innerText = ""
-  document.getElementById("signedup-msg").innerText = ""
-  const userNameInput = document.getElementById("username")
-  const passwordInput = document.getElementById("password")
-  const loginRequest = {}
-  loginRequest.username = userNameInput.value
-  loginRequest.password = passwordInput.value
-  const options = {
-    method: "POST",
-    headers: { "Content-type": "application/json" },
-    body: JSON.stringify(loginRequest)
-  }
-  try {
-    const res = await fetch(API_URL + "/auth/login", options).then(r => handleHttpErrors(r))
-    storeLoginDetails(res)
-    window.router.navigate("/")
-
-  } catch (err) {
-    responseStatus.style.color = "darkred"
-    if (err.apiError) {
-      responseStatus.innerText = err.apiError.message
-    } else {
-      responseStatus.innerText = err.message
-    }
-  }
+	responseStatus.innerText = "";
+	const userNameInput = document.getElementById("username");
+	const passwordInput = document.getElementById("password");
+	const loginRequest = {};
+	loginRequest.username = userNameInput.value;
+	loginRequest.password = passwordInput.value;
+	const options = {
+		method: "POST",
+		headers: { "Content-type": "application/json" },
+		body: JSON.stringify(loginRequest),
+	};
+	try {
+		const res = await fetch(API_URL + "/auth/login", options).then((r) =>
+			handleHttpErrors(r)
+		);
+		storeLoginDetails(res);
+		window.router.navigate("/");
+	} catch (err) {
+		responseStatus.style.color = "darkred";
+		if (err.apiError) {
+			responseStatus.innerText = err.apiError.message;
+		} else {
+			responseStatus.innerText = err.message;
+		}
+	}
 }
 export function logout() {
-  localStorage.removeItem("token")
-  localStorage.removeItem("user")
-  localStorage.removeItem("roles")
-  toggleLoginStatus(false)
+	localStorage.removeItem("token");
+	localStorage.removeItem("user");
+	localStorage.removeItem("roles");
+	toggleLoginStatus(false);
 }
 
 export function toggleLoginStatus(loggedIn) {
-  //Hide or show all menu-links depending on login status
-  const adminListItems = document.querySelectorAll(".admin-only")
-  let isAdmin = false
-  let isUser = false
-  //You can add .getitem and change [] to () and it would still work in the if statement below.
-  if (localStorage["roles"]){ 
-    isAdmin = localStorage.getItem("roles").includes("admin")
-    isUser = localStorage.getItem("roles").includes("user") 
-  }
-  for(let i=0; i < adminListItems.length; i++ ){
-    adminListItems[i].style.display = isAdmin ? "block" : "none"
-  }
-  document.getElementById("login-container").style.display = loggedIn ? "none" : "block"
-  document.getElementById("logout-container").style.display = loggedIn ? "block" : "none"
-  document.getElementById("signup-container").style.display = loggedIn ? "none" : "block"
-  const loggedInUserTxt = loggedIn ? `User: ${localStorage["user"]} (${localStorage["roles"]})` : ""
-  document.getElementById("user-details").innerText = loggedInUserTxt
-  if(responseStatus){
-    responseStatus.innerText = ""
-  } 
+	//Hide or show all menu-links depending on login status
+	const adminListItems = document.querySelectorAll(".admin-only");
+	let isAdmin = false;
+	let isUser = false;
+	//You can add .getitem and change [] to () and it would still work in the if statement below.
+	if (localStorage["roles"]) {
+		isAdmin = localStorage.getItem("roles").includes("ADMIN");
+		isUser = localStorage.getItem("roles").includes("USER");
+	}
+	for (let i = 0; i < adminListItems.length; i++) {
+		adminListItems[i].style.display = isAdmin ? "block" : "none";
+	}
+	document.getElementById("login-container").style.display = loggedIn
+		? "none"
+		: "block";
+	document.getElementById("logout-container").style.display = loggedIn
+		? "block"
+		: "none";
+	document.getElementById("signup-container").style.display = loggedIn
+		? "none"
+		: "block";
+	const loggedInUserTxt = loggedIn
+		? `User: ${localStorage["user"]} (${localStorage["roles"]})`
+		: "";
+	document.getElementById("user-details").innerText = loggedInUserTxt;
+	if (responseStatus) {
+		responseStatus.innerText = "";
+	}
 }
 
 /**
@@ -73,9 +81,9 @@ export function toggleLoginStatus(loggedIn) {
  * @param res - Response object with details provided by server for a succesful login
  */
 function storeLoginDetails(res) {
-  localStorage.setItem("token", res.token)
-  localStorage.setItem("user", res.username)
-  localStorage.setItem("roles", res.roles)
-  //Update UI
-  toggleLoginStatus(true)
+	localStorage.setItem("token", res.token);
+	localStorage.setItem("user", res.username);
+	localStorage.setItem("roles", res.roles);
+	//Update UI
+	toggleLoginStatus(true);
 }
--- pages/product-overview/product-overview.js
+++ pages/product-overview/product-overview.js
@@ -1,61 +1,64 @@
 import { API_URL, SAS_TOKEN } from "../../settings.js";
 
 export async function initProductOverviewPage(searchTerm) {
-  console.log("Search term on product overview:", searchTerm);
+	console.log("Search term on product overview:", searchTerm);
 
+	if (searchTerm) {
+		const searchResults = await getSearchItems(searchTerm);
+		fetchAndDisplayProducts(searchResults);
+	} else {
+		fetchAndDisplayProducts();
+	}
 
-  if (searchTerm) {
-    const searchResults = await getSearchItems(searchTerm);
-    fetchAndDisplayProducts(searchResults);
-  } else {
-    fetchAndDisplayProducts()
-  }
+	addEventBubbling();
+}
 
-  async function getSearchItems(searchTerm) {
-    try {
-      const res = await fetch(API_URL + "/products/detailed");
-      const data = await res.json();
+async function getSearchItems(searchTerm) {
+	try {
+		const res = await fetch(API_URL + "/products/detailed");
+		const data = await res.json();
 
-      return data.filter((product) => {
-        const term = searchTerm.toLowerCase();
-        return (
-          product.category.toLowerCase().includes(term) ||
-          product.name.toLowerCase().includes(term) ||
-          product.description.toLowerCase().includes(term)
-        );
-      });
-    } catch (error) {
-      console.error("Error in getSearchItems:", error);
-    }
-  }
+		return data.filter((product) => {
+			const term = searchTerm.toLowerCase();
+			return (
+				product.category.toLowerCase().includes(term) ||
+				product.name.toLowerCase().includes(term) ||
+				product.description.toLowerCase().includes(term)
+			);
+		});
+	} catch (error) {
+		console.error("Error in getSearchItems:", error);
+	}
 }
 
 async function fetchAndDisplayProducts(products) {
-  //console.log("Fetching products...");
+	//console.log("Fetching products...");
 
-  if (!products) {
-    try {
-      const response = await fetch(API_URL+"/products");
-      products = await response.json();
-    } catch (error) {
-      console.error("Error fetching products:", error);
-      return;
-    }
-  }
-  const productList = document.getElementById("productList");
+	if (!products) {
+		try {
+			const response = await fetch(API_URL + "/products/detailed");
+			products = await response.json();
+		} catch (error) {
+			console.error("Error fetching products:", error);
+			return;
+		}
+	}
+	const productList = document.getElementById("productList");
 
-  productList.innerHTML = ""; // Clear previous content
-  productList.classList.add("row"); // Add Bootstrap row class for grid layout
-  console.log(products);
-  products.forEach((product) => {
-    const productDiv = document.createElement("div");
-    productDiv.classList.add("col-md-4", "mb-3"); // Bootstrap grid column and margin-bottom
-    console.log(product.imageUrl);
-    productDiv.innerHTML = `
-        <div class="card">
-          <img src="${product.imageUrls[0] + SAS_TOKEN}" class="card-img-top" alt="${
-      product.name
-    }" style="width:100%; height:auto;">
+	productList.innerHTML = ""; // Clear previous content
+	productList.classList.add("row"); // Add Bootstrap row class for grid layout
+	console.log(products);
+	products.forEach((product) => {
+		const productDiv = document.createElement("div");
+		productDiv.classList.add("col-md-4", "mb-3"); // Bootstrap grid column and margin-bottom
+		console.log(product.imageUrl);
+		productDiv.innerHTML = `
+        <div class="card" data-id="${product.id}">
+          <img src="${
+						product.imageUrls[0] + SAS_TOKEN
+					}" class="card-img-top" alt="${
+			product.name
+		}" style="width:100%; height:auto;">
           <div class="card-body">
             <h5 class="card-title">${product.name}</h5>
             <p class="card-text">Pris: ${product.price.toFixed(0)}.- kr</p>
@@ -63,6 +66,19 @@ async function fetchAndDisplayProducts(products) {
         </div>
       `;
 
-    productList.appendChild(productDiv);
-  });
+		productList.appendChild(productDiv);
+	});
+}
+
+function addEventBubbling() {
+	document.getElementById("productList").addEventListener("click", (event) => {
+		// Find the closest parent with a data-id attribute
+		const productCard = event.target.closest("[data-id]");
+
+		if (productCard) {
+			const productId = productCard.dataset.id;
+			// Now you can use productId to navigate or perform other actions
+			window.router.navigate(`/product-page/${productId}`);
+		}
+	});
 }
